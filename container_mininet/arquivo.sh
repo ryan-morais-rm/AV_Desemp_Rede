acbb5e9d08baf6d2fab5a5a3d7698c8e4b43387e
Ryan de Morais Correia <ryan.morais.workspace@gmail.com>
2025-06-12 21:55:41 -0300

    Alteração do arquivo de bash para ficar mais claro do que fazer

--- container_mininet/arquivo.sh
+++ container_mininet/arquivo.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 # Criação das redes privadas com blocos definidos.
-docker network create -d bridge --subnet=172.19.0.0/16 brteste02
-docker network create -d bridge --subnet=172.18.0.0/16 brteste01
+docker network create -d bridge --subnet=172.19.0.0/16 --gateway=172.19.0.10 brteste02
+docker network create -d bridge --subnet=172.18.0.0/16 --gateway=172.18.0.10 brteste01 
 
 # Rodando os containers client e server.
-docker run --name server --network brteste02 --ip 172.19.0.2 –cap-add=NET_ADMIN -dit image_ubuntu:latest
-docker run --name client --network brteste01 --ip 172.18.0.2 –cap-add=NET_ADMIN -dit image_ubuntu:latest
+docker run --name server --network brteste02 --ip 172.19.0.2 --cap-add=NET_ADMIN -dit image_ubuntu:v1.0
+docker run --name client --network brteste01 --ip 172.18.0.2 --cap-add=NET_ADMIN -dit image_ubuntu:v1.0
 
 # Adicionando rotas, server e client, respectivamente.
-ip route add 172.18.0.0/16 via 172.19.0.10
-ip route add 172.19.0.0/16 via 172.18.0.10
+server -> ip route add 172.18.0.0/16 via 172.19.0.10
+client -> ip route add 172.19.0.0/16 via 172.18.0.10
